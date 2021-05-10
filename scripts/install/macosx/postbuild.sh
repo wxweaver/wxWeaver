765d073a2af7ca63cd43fa122cdf4d93cbd6d018
redtide <redtid3@gmail.com>
2021-05-10 10:11:28 +0200

    WIP: Remove macOS postbuild scripts (we will use CMake)

--- scripts/install/macosx/postbuild.sh
+++ scripts/install/macosx/postbuild.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #*****************************************************************************
-#* Copyright (C) 2007 Auria (as wxFormBuilder)
-#* Copyright (C) 2007 Andrea Zanellato
+#* Copyright (C) 2007 Auria
 #*
 #* This program is free software; you can redistribute it and/or
 #* modify it under the terms of the GNU General Public License
